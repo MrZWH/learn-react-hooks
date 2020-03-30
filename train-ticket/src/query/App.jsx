8a9173749cecb0163bff5f01739750061f711edf
MrZWH <blesszhang@foxmail.com>
2020-03-30 13:30:52 +0800

    搜索结果页开发

--- train-ticket/src/query/App.jsx
+++ train-ticket/src/query/App.jsx
@@ -1,9 +1,9 @@
-import React, {useCallback,useEffect,} from 'react';
+import React, { useCallback, useEffect, } from 'react';
 import { connect } from 'react-redux'
 import URI from 'urijs';
 import dayjs from 'dayjs'
 
-import {h0} from '../common/fp';
+import { h0 } from '../common/fp';
 import Header from '../common/Header.jsx'
 import Nav from '../common/Nav.jsx'
 import List from './List.jsx'
@@ -28,34 +28,31 @@ import './App.css';
 
 function App(props) {
   const {
+    trainList,
     from,
     to,
+    departDate,
     searchParsed,
-    dispatch
-  } =  props;
+    dispatch,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  } = props;
 
   useEffect(() => {
     const queries = URI.parseQuery(window.location.search)
     const {
       from,
       to,
-      departDate,
+      date,
       highSpeed,
-        orderType,
-        onlyTickets,
-        isFiltersVisible,
-        ticketTypes,
-        trainTypes,
-        departStations,
-        arriveStations,
-        checkedTicketTypes,
-        checkedTrainTypes,
-        checkedDepartStations,
-        checkedArriveStations,
-        departTimeStart,
-        departTimeEnd,
-        arriveTimeStart,
-        arriveTimeEnd,
     } = queries;
 
     dispatch(setFrom(from))
@@ -67,78 +64,79 @@ function App(props) {
   }, [])
 
   useEffect(() => {
-    if(!searchParsed) {
+    if (!searchParsed) {
       return;
     }
 
     const url = new URI('/rest/query')
-            .setSearch('from', from)
-            .setSearch('to', to)
-            .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
-            .setSearch('highSpeed', highSpeed)
-            .setSearch('orderType', orderType)
-            .setSearch('onlyTickets', onlyTickets)
-            .setSearch(
-                'checkedTicketTypes',
-                Object.keys(checkedTicketTypes).join()
-            )
-            .setSearch(
-                'checkedTrainTypes',
-                Object.keys(checkedTrainTypes).join()
-            )
-            .setSearch(
-                'checkedDepartStations',
-                Object.keys(checkedDepartStations).join()
-            )
-            .setSearch(
-                'checkedArriveStations',
-                Object.keys(checkedArriveStations).join()
-            )
-            .setSearch('departTimeStart', departTimeStart)
-            .setSearch('departTimeEnd', departTimeEnd)
-            .setSearch('arriveTimeStart', arriveTimeStart)
-            .setSearch('arriveTimeEnd', arriveTimeEnd)
-            .toString();
+      .setSearch('from', from)
+      .setSearch('to', to)
+      .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+      .setSearch('highSpeed', highSpeed)
+      .setSearch('orderType', orderType)
+      .setSearch('onlyTickets', onlyTickets)
+      .setSearch(
+        'checkedTicketTypes',
+        Object.keys(checkedTicketTypes).join()
+      )
+      .setSearch(
+        'checkedTrainTypes',
+        Object.keys(checkedTrainTypes).join()
+      )
+      .setSearch(
+        'checkedDepartStations',
+        Object.keys(checkedDepartStations).join()
+      )
+      .setSearch(
+        'checkedArriveStations',
+        Object.keys(checkedArriveStations).join()
+      )
+      .setSearch('departTimeStart', departTimeStart)
+      .setSearch('departTimeEnd', departTimeEnd)
+      .setSearch('arriveTimeStart', arriveTimeStart)
+      .setSearch('arriveTimeEnd', arriveTimeEnd)
+      .toString();
 
-        fetch(url)
-          .then(response => response.json())
-          .then(result => {
-            const {
-              dateMap: {
-                directTrainInfo: {
-                  trains,
-                  filetr: {
-                    ticketType,trainType,
-                    depStation,
-                    arrStation,
-                  }
-                }
+    fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        const {
+          dateMap: {
+            directTrainInfo: {
+              trains,
+              filetr: {
+                ticketType,
+                trainType,
+                depStation,
+                arrStation,
               }
-            } = result;
+            }
+          }
+        } = result;
 
-            dispatch(setTrainList(trains));
-                dispatch(setTicketTypes(ticketType));
-                dispatch(setTrainTypes(trainType));
-                dispatch(setDepartStations(depStation));
-                dispatch(setArriveStations(arrStation));
-          })
+        dispatch(setTrainList(trains));
+        dispatch(setTicketTypes(ticketType));
+        dispatch(setTrainTypes(trainType));
+        dispatch(setDepartStations(depStation));
+        dispatch(setArriveStations(arrStation));
+      })
   }, [
-        from,
-        to,
-        departDate,
-        highSpeed,
-        searchParsed,
-        orderType,
-        onlyTickets,
-        checkedTicketTypes,
-        checkedTrainTypes,
-        checkedDepartStations,
-        checkedArriveStations,
-        departTimeStart,
-        departTimeEnd,
-        arriveTimeStart,
-        arriveTimeEnd,
-      ])
+    from,
+    to,
+    departDate,
+    highSpeed,
+    searchParsed,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  ])
 
   const onBack = useCallback(() => {
     window.history.back()
@@ -162,16 +160,16 @@ function App(props) {
   return (
     <div className="App">
       <div className="header-wrapper">
-      <Header title={`${from} → ${to}`} onBack={onBack}/>
-      <Nav
-        date={departDate}
-        isPrevDisabled={isPrevDisabled}
-        isNextDisabled={isNextDisabled}
-        prev={prev}
-        next={next}
-      />
-      <List/>
-      <Bottom/>
+        <Header title={`${from} → ${to}`} onBack={onBack} />
+        <Nav
+          date={departDate}
+          isPrevDisabled={isPrevDisabled}
+          isNextDisabled={isNextDisabled}
+          prev={prev}
+          next={next}
+        />
+        <List list={trainList} />
+        <Bottom />
       </div>
     </div>
   );
@@ -182,6 +180,6 @@ export default connect(
     return state
   },
   function mapDispatchtoProps(dispatch) {
-    return {dispatch}
+    return { dispatch }
   }
 )(App);
--- train-ticket/src/query/List.jsx
+++ train-ticket/src/query/List.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import URI from 'urijs';
 import './List.css'
 
-export default function List(props) {
-	return null
-}
+const ListItem = memo(function ListItem(props) {
+	const {
+		dTime,
+		aTime,
+		dStation,
+		aStation,
+		trainNumber,
+		date,
+		time,
+		priceMsg,
+		dayAfter,
+	} = props;
+
+	const url = useMemo(() => {
+		return new URI('ticket.html')
+			.setSearch('aStation', aStation)
+			.setSearch('dStation', dStation)
+			.setSearch('trainNumber', trainNumber)
+			.setSearch('date', date)
+			.toString()
+	}, [
+		aStation,
+		dStation,
+		trainNumber,
+		date,
+	])
+
+	return (
+		<li className="list-item">
+			<a href={url}>
+				<span className="item-time">
+					<em>{dTime}</em>
+					<br />
+					<em className="em-light">{aTime} <i className="time-after">{dayAfter}</i>	</em>
+				</span>
+				<span className="item-station">
+					<em>
+						<i className="train-station train-start">始</i>
+						{dStation}
+					</em>
+					<br />
+					<em className="em-light">
+						<i className="train-station train-end">终</i>
+						{aStation}
+					</em>
+				</span>
+				<span className="item-train">
+					<em>{trainNumber}</em>
+					<br />
+					<em className="em-light">{time}</em>
+				</span>
+				<span className="item-ticket">
+					<em>{priceMsg}</em>
+					<br />
+					<em className="em-light-orange">可抢票</em>
+				</span>
+			</a>
+		</li>
+	)
+})
+
+const List = memo(function List(props) {
+	return (
+		<ul className="list">
+			{
+				list.map(item => <ListItem {...item} key={item.trainNumber} />)
+			}
+		</ul>
+	)
+})
 
 List.propTypes = {
-	
+	dTime: PropTypes.string.isRequired,
+	aTime: PropTypes.string.isRequired,
+	dStation: PropTypes.string.isRequired,
+	aStation: PropTypes.string.isRequired,
+	trainNumber: PropTypes.string.isRequired,
+	date: PropTypes.string.isRequired,
+	time: PropTypes.string.isRequired,
+	priceMsg: PropTypes.string.isRequired,
+	dayAfter: PropTypes.string.isRequired,
 }
+
+export default List
